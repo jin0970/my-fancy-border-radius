d1c499540ca60bb6874985ad3fe6cef3cbaa972e
jin0970 <grahambarbra6@gmail.com>
2026-01-04 23:35:23 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-fancy-border-radius/',
   plugins: [
     vue(),
     vueDevTools(),
